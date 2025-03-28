bcd6a216594103cda1f7e0205ce1be15f7d7974e
AlejandroDanielMorales <116897330+AlejandroDanielMorales@users.noreply.github.com>
2025-03-27 23:41:33 -0300

    correcciones responsivas

--- src/assets/components/ServicesList/ServicesList.jsx
+++ src/assets/components/ServicesList/ServicesList.jsx
@@ -31,7 +31,19 @@ const services = [
 
 export default function ServicesList() {
     return (
-        <section id="services-section" className="services">
+        <section id="services" className="services">
+             <div className="services-header">
+             <h1>Mutants Technologies</h1>
+                <h2>Los mejores amigos de tu PC</h2>
+
+                <h2>¿Quiénes Somos?</h2>
+                <p>
+                    En nuestro corazón late una profunda pasión por el gaming y la tecnología. Somos un equipo diverso de entusiastas, gamers y expertos en hardware que se unen con un objetivo claro: ofrecer a cada jugador la mejor experiencia posible. Nos dedicamos a proporcionar los mejores componentes para PC, abarcando desde el hardware esencial hasta los periféricos más avanzados.
+                </p>
+
+             </div>
+                
+
             <h2>Nuestros Servicios</h2>
             <div className="card-container">
                 {services.map((service, index) => (
