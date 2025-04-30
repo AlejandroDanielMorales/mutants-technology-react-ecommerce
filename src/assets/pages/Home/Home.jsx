f01bb0792663e71a93f733b44d7f3c03aa828208
AlejandroDanielMorales <Alejandrod.morales@hotmail.com>
2025-04-30 01:16:17 -0300

    Update Home.jsx

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import Banner from '../../components/Banner/Banner';
 import Features from '../../components/Features/Features';
 import Categories from '../../components/Categories/Categories';
@@ -6,13 +7,18 @@ import SearchBar from '../../components/SearchBar/SearchBar';
 import ProductsList from '../../components/ProductsList/ProductsList';
 import { useCategories } from '../../context/CategoryProvider';
 import { useOrder } from '../../context/OrderContext';
+import { useUser } from '../../context/UserProvider';
 
 export default function Home() {
   const { categories } = useCategories();
   const { products} = useOrder();
+  const{ fechCurrentUser } = useUser(); 
   
 
-
+  // Fetch current user on component mount
+useEffect(() => {
+    fechCurrentUser();
+  }, []);
 
   const filterProductsByCategory = (categoryName) => {
     return products.filter(product => product.category.toLowerCase() === categoryName.toLowerCase());
