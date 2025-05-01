83b149c3bb5abb703b9f5d7eb5f1dd5535d95d58
AlejandroDanielMorales <Alejandrod.morales@hotmail.com>
2025-05-01 13:20:48 -0300

    add spinner

--- src/assets/pages/Home/Home.jsx
+++ src/assets/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Banner from '../../components/Banner/Banner';
 import Features from '../../components/Features/Features';
 import Categories from '../../components/Categories/Categories';
@@ -8,15 +7,16 @@ import ProductsList from '../../components/ProductsList/ProductsList';
 import { useCategories } from '../../context/CategoryProvider';
 import { useOrder } from '../../context/OrderContext';
 import { useUser } from '../../context/UserProvider';
+import Spinner from '../../components/Spinner/Spinner';
 
 export default function Home() {
   const { categories } = useCategories();
-  const { products} = useOrder();
-  const{ fechCurrentUser } = useUser(); 
-  
+  const { products } = useOrder();
+  const { fechCurrentUser } = useUser();
 
-  // Fetch current user on component mount
-useEffect(() => {
+  const isLoading = !categories || !products || categories.length === 0 || products.length === 0;
+
+  useEffect(() => {
     fechCurrentUser();
   }, []);
 
@@ -30,17 +30,20 @@ useEffect(() => {
       <SearchBar />
       <Categories />
 
-      {categories.map((cat) => {
-        const filteredProducts = filterProductsByCategory(cat.name);
-        console.log(cat._id);
-        return filteredProducts.length > 0 ? (
-          <ProductsList
-            refkey={cat._id}
-            title={cat.description || cat.name}
-            products={filteredProducts}
-          />
-        ) : null;
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        categories.map((cat) => {
+          const filteredProducts = filterProductsByCategory(cat.name);
+          return filteredProducts.length > 0 ? (
+            <ProductsList
+              key={cat._id}
+              title={cat.description || cat.name}
+              products={filteredProducts}
+            />
+          ) : null;
+        })
+      )}
 
       <Features />
     </>
